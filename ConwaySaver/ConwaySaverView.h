088f79a1ee61fac689ba85e7b15cf91942ca5c39
Michael Schade <github@mschade.me>
2014-07-04 02:44:23 -0700

    Initial Commit

---
+++ ConwaySaver/ConwaySaverView.h
@@ -0,0 +1,13 @@
+//
+//  ConwaySaverView.h
+//  ConwaySaver
+//
+//  Created by Michael Schade on 7/4/14.
+//  Copyright (c) 2014 Michael Schade. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ConwaySaverView : ScreenSaverView
+
+@end
---
+++ ConwaySaver/ConwaySaverView.m
@@ -0,0 +1,52 @@
+//
+//  ConwaySaverView.m
+//  ConwaySaver
+//
+//  Created by Michael Schade on 7/4/14.
+//  Copyright (c) 2014 Michael Schade. All rights reserved.
+//
+
+#import "ConwaySaverView.h"
+
+@implementation ConwaySaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
